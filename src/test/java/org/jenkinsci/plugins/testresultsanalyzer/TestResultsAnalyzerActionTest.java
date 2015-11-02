49b4901bb1cbda29d2d1f9892097b891c8bc3f60
smiqbal2 <smiqbal2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-02 21:57:09 +0000

    Added one test case to help debug getLastTwoBuilds method

--- src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
+++ src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
@@ -2,6 +2,9 @@ package org.jenkinsci.plugins.testresultsanalyzer;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -11,33 +14,56 @@ import hudson.model.FreeStyleProject;
 import net.sf.json.JSONObject;
 
 public class TestResultsAnalyzerActionTest {
-	@Rule public JenkinsRule j = new JenkinsRule();
+	@Rule
+	public JenkinsRule j = new JenkinsRule();
 
-	@Test public void testGetTreeResult1() throws Exception {
+	@Test
+	public void testGetTreeResult1() throws Exception {
 		FreeStyleProject project = j.createFreeStyleProject();
 		FreeStyleBuild build = project.scheduleBuild2(0).get();
 		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
-		
-		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(project);
+
+		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
+				project);
 		action.getJsonLoadData();
 		JSONObject jsonObj = action.getTreeResult("2", "PASSED");
 		System.out.println(jsonObj.toString());
-		assertEquals("{\"builds\":[\"2\",\"1\"],\"results\":[]}", jsonObj.toString());
+		assertEquals("{\"builds\":[\"2\",\"1\"],\"results\":[]}",
+				jsonObj.toString());
 	}
-  
-	@Test public void testGetTreeResult2() throws Exception {
+
+	@Test
+	public void testGetTreeResult2() throws Exception {
 		FreeStyleProject project = j.createFreeStyleProject();
 		FreeStyleBuild build = project.scheduleBuild2(0).get();
 		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
 		FreeStyleBuild build3 = project.scheduleBuild2(0).get();
 		FreeStyleBuild build4 = project.scheduleBuild2(0).get();
-		
-		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(project);
+
+		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
+				project);
 		action.getJsonLoadData();
-	  	
+
 		JSONObject jsonObj = action.getTreeResult("2", "all");
-		assertEquals("{\"builds\":[\"4\",\"3\"],\"results\":[]}", jsonObj.toString());
+		assertEquals("{\"builds\":[\"4\",\"3\"],\"results\":[]}",
+				jsonObj.toString());
 		jsonObj = action.getTreeResult("5", "all");
-		assertEquals("{\"builds\":[\"4\",\"3\",\"2\",\"1\"],\"results\":[]}", jsonObj.toString());
-	}	
+		assertEquals("{\"builds\":[\"4\",\"3\",\"2\",\"1\"],\"results\":[]}",
+				jsonObj.toString());
+	}
+
+	@Test
+	public void testGetLastTwoBuilds() throws Exception {
+		FreeStyleProject project = j.createFreeStyleProject();
+		FreeStyleBuild build = project.scheduleBuild2(0).get();
+		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
+		FreeStyleBuild build3 = project.scheduleBuild2(0).get();
+		FreeStyleBuild build4 = project.scheduleBuild2(0).get();
+
+		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
+				project);
+		action.getJsonLoadData();
+		
+		System.out.println(action.getLastTwoBuilds());
+	}
 }
