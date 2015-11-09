f13a3c9d4511996cde3dffdd00a641adb0eb5b83
yyue7 <yyue7@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-09 20:52:38 +0000

    modified selectdiffresults.js to add two dropdown menus to select build numbers for build comparision

--- src/main/webapp/js/selectdiffresults.js
+++ src/main/webapp/js/selectdiffresults.js
@@ -1,16 +1,15 @@
 // Creates the dropdown menus that allows the user to select which two builds they want to compare.
 
 function createDropdown(resultData) {
-    //var menus = $j('Compare build <select id="selectFirstBuild"></select> with build <select id="selectSecondBuild"></select>');
-    var menus = $j('<select />');
+    var select = '';
     var numBuilds = resultData['builds'].length;
-
     for (var i = 0; i < numBuilds; i++) {
-        var curBuildNumber = resultData['builds'][i];
-        var option = $j('<option value="' + curBuildNumber + '">' + curBuildNumber + '</option');
-        console.log(option);
-        menus.append(option);
+        var buildNumber = resultData['builds'][i];
+        var option = '<option value="'+buildNumber+'">'+buildNumber+'</option>';
+        select = select + option;
     }
+    select = select + '</select>';
 
+    var menus = $j('<span>Compare build <select id="selectFirstBuild">'+select+' with build <select id="selectSecondBuild">'+select+'.</span>');
     return menus;
 }
