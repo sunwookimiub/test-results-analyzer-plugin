c940a8f5f7fea525754fde5275b61e86fa6577f7
kim392 <kim392@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-13 21:10:05 +0000

    Added checkboxes next to each build number in the table header (kim392/czhang49)

--- src/main/webapp/js/test-result-analyzer-template.js
+++ src/main/webapp/js/test-result-analyzer-template.js
@@ -27,7 +27,7 @@ var tableContent = '<div class="table-row {{parentclass}}-{{addName text}}" pare
 var tableBody = '<div class="heading">' +
     '\n' + '        <div class="table-cell">Chart</div><div class="table-cell">See children</div> <div class="table-cell">The Condensed Results</div>' +
     '{{#each builds}}' +
-    '\n' + '         <div class="table-cell">{{this}}</div>' +
+    '\n' + '         <div class="table-cell">{{this}}<input type="checkbox" buildId={{this}} {{#if isChecked}}checked{{/if}}/></div>' +
     '{{/each}}' +
     '\n' + '      </div>' +
     '{{#each results}}' +
