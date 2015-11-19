5af314363ca8ace31a9bca021f33fa32b35454e5
czhang49 <czhang49@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-11-19 22:24:18 +0000

    modification on boxcheck

--- _projects/Banana/test-results-analyzer-plugin/trunk/src/test/webapp/jasmine/spec/CheckBoxSelectBuildsSpec.js
+++ _projects/Banana/test-results-analyzer-plugin/trunk/src/test/webapp/jasmine/spec/CheckBoxSelectBuildsSpec.js
@@ -66,12 +66,12 @@ describe("CheckBox Event Tests", function() {
   // This checks if the selected boxes are selected
  it ("should check the selected boxes", function() {
     // This following two methods are basically doing the same thing
-//    $('#1').trigger( "click" );
+    $('#1').trigger( "click" );
     //spyEvent = spyOnEvent('#1', 'click');
 
-//    expect('click').toHaveBeenTriggeredOn("#1");
+    expect('click').toHaveBeenTriggeredOn("#1");
     //expect(spyEvent).toHaveBeenTriggered();
-    expect(1).toEqual(1);
+//    expect(1).toEqual(1);
   });
 
   // This checks if click on compare results, the needed builds are shown.
