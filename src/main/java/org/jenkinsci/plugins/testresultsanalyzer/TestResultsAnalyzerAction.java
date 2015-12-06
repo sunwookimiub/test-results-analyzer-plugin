4d22fea17e9513b3c11d96a889780fac4d16e043
sapin2 <sapin2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-06 18:13:53 +0000

    convert mixxed tab/space indentation to spaces

--- src/main/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerAction.java
+++ src/main/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerAction.java
@@ -28,14 +28,14 @@ import org.kohsuke.stapler.bind.JavaScriptMethod;
 
 public class TestResultsAnalyzerAction extends Actionable implements Action{
 @SuppressWarnings("rawtypes") AbstractProject project;
-	private List<Integer> builds = new ArrayList<Integer>() ;
+    private List<Integer> builds = new ArrayList<Integer>() ;
 
-	ResultInfo resultInfo;
+    ResultInfo resultInfo;
 
 
-	public TestResultsAnalyzerAction(@SuppressWarnings("rawtypes") AbstractProject project){
-		this.project = project;
-	}
+    public TestResultsAnalyzerAction(@SuppressWarnings("rawtypes") AbstractProject project){
+        this.project = project;
+    }
 
     /**
      * Gets all the dependant jobs of the AbstractProject. If in a module already
@@ -64,7 +64,7 @@ public class TestResultsAnalyzerAction extends Actionable implements Action{
         return ret;
     }
 
-	/**
+    /**
      * The display name for the action.
      *
      * @return the name as String
@@ -110,78 +110,78 @@ public class TestResultsAnalyzerAction extends Actionable implements Action{
     }
 
     @SuppressWarnings("rawtypes")
-	public AbstractProject getProject(){
-    	return this.project;
+    public AbstractProject getProject(){
+        return this.project;
     }
 
 
-	@JavaScriptMethod
-	public JSONArray getNoOfBuilds(String noOfbuildsNeeded) {
-		JSONArray jsonArray;
-		int noOfBuilds = getNoOfBuildRequired(noOfbuildsNeeded);
+    @JavaScriptMethod
+    public JSONArray getNoOfBuilds(String noOfbuildsNeeded) {
+        JSONArray jsonArray;
+        int noOfBuilds = getNoOfBuildRequired(noOfbuildsNeeded);
 
-		jsonArray = getBuildsArray(getBuildList(noOfBuilds));
+        jsonArray = getBuildsArray(getBuildList(noOfBuilds));
 
-		return jsonArray;
-	}
+        return jsonArray;
+    }
 
-	private JSONArray getBuildsArray(List<Integer> buildList) {
-		JSONArray jsonArray = new JSONArray();
-		for (Integer build : buildList) {
-			jsonArray.add(build);
-		}
-		return jsonArray;
-	}
+    private JSONArray getBuildsArray(List<Integer> buildList) {
+        JSONArray jsonArray = new JSONArray();
+        for (Integer build : buildList) {
+            jsonArray.add(build);
+        }
+        return jsonArray;
+    }
 
-	private List<Integer> getBuildList(int noOfBuilds) {
-		if ((noOfBuilds <= 0) || (noOfBuilds >= builds.size())) {
-			return builds;
-		}
-		List<Integer> buildList = new ArrayList<Integer>();
-		for (int i = 0; i <= (noOfBuilds - 1); i++) {
-			buildList.add(builds.get(i));
-		}
-		return buildList;
-	}
+    private List<Integer> getBuildList(int noOfBuilds) {
+        if ((noOfBuilds <= 0) || (noOfBuilds >= builds.size())) {
+            return builds;
+        }
+        List<Integer> buildList = new ArrayList<Integer>();
+        for (int i = 0; i <= (noOfBuilds - 1); i++) {
+            buildList.add(builds.get(i));
+        }
+        return buildList;
+    }
 
-	private int getNoOfBuildRequired(String noOfbuildsNeeded){
-		int noOfBuilds;
-		try {
-			noOfBuilds = Integer.parseInt(noOfbuildsNeeded);
-		} catch (NumberFormatException e) {
-			noOfBuilds = -1;
-		}
-		return noOfBuilds;
-	}
+    private int getNoOfBuildRequired(String noOfbuildsNeeded){
+        int noOfBuilds;
+        try {
+            noOfBuilds = Integer.parseInt(noOfbuildsNeeded);
+        } catch (NumberFormatException e) {
+            noOfBuilds = -1;
+        }
+        return noOfBuilds;
+    }
 
-	public boolean isUpdated(){
-		int latestBuildNumber = project.getLastBuild().getNumber();
-		return !(builds.contains(latestBuildNumber));
-	}
+    public boolean isUpdated(){
+        int latestBuildNumber = project.getLastBuild().getNumber();
+        return !(builds.contains(latestBuildNumber));
+    }
 
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public void getJsonLoadData() {
-		if (isUpdated()) {
-			resultInfo = new ResultInfo();
-			builds = new ArrayList<Integer>();
-			RunList<Run> runs = project.getBuilds();
-			Iterator<Run> runIterator = runs.iterator();
-			while (runIterator.hasNext()) {
-				Run run = runIterator.next();
-				int buildNumber = run.getNumber();
-				builds.add(run.getNumber());
-				List<AbstractTestResultAction> testActions = run.getActions(hudson.tasks.test.AbstractTestResultAction.class);
-				for (hudson.tasks.test.AbstractTestResultAction testAction : testActions) {
-					TestResult testResult = (TestResult) testAction.getResult();
-					Collection<PackageResult> packageResults = testResult.getChildren();
-					for (PackageResult packageResult : packageResults) { // packageresult
-						resultInfo.addPackage(buildNumber, packageResult);
-					}
-				}
-			}
-		}
-	}
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    public void getJsonLoadData() {
+        if (isUpdated()) {
+            resultInfo = new ResultInfo();
+            builds = new ArrayList<Integer>();
+            RunList<Run> runs = project.getBuilds();
+            Iterator<Run> runIterator = runs.iterator();
+            while (runIterator.hasNext()) {
+                Run run = runIterator.next();
+                int buildNumber = run.getNumber();
+                builds.add(run.getNumber());
+                List<AbstractTestResultAction> testActions = run.getActions(hudson.tasks.test.AbstractTestResultAction.class);
+                for (hudson.tasks.test.AbstractTestResultAction testAction : testActions) {
+                    TestResult testResult = (TestResult) testAction.getResult();
+                    Collection<PackageResult> packageResults = testResult.getChildren();
+                    for (PackageResult packageResult : packageResults) { // packageresult
+                        resultInfo.addPackage(buildNumber, packageResult);
+                    }
+                }
+            }
+        }
+    }
     @JavaScriptMethod
     public JSONObject getTreeResult(String noOfBuildsNeeded, String statusFilter) {
         int noOfBuilds = getNoOfBuildRequired(noOfBuildsNeeded);
@@ -191,11 +191,11 @@ public class TestResultsAnalyzerAction extends Actionable implements Action{
         return jsTreeUtils.getJsTree(buildList, resultInfo, statusFilter);
     }
 //<<<<<<< .mine
-		@JavaScriptMethod
+        @JavaScriptMethod
     public JSONObject getTreeResultCondensed(String noOfBuildsNeeded, String statusFilter) {
         int noOfBuilds = getNoOfBuildRequired(noOfBuildsNeeded);
         List<Integer> buildList = getBuildList(noOfBuilds);
-				List<Integer> buildCondense = getBuildList(3);
+                List<Integer> buildCondense = getBuildList(3);
 
         JsTreeUtil jsTreeUtils = new JsTreeUtil();
         return jsTreeUtils.getJsTreeCondensed(buildCondense,buildList, resultInfo, statusFilter);
@@ -203,26 +203,26 @@ public class TestResultsAnalyzerAction extends Actionable implements Action{
 //=======
 
     public String getLastTwoBuilds() {
-    	String ret = "Last two builds: \n";
-    	Map<String,PackageInfo> results = resultInfo.getPackageResults();
-    	Iterator it = results.keySet().iterator();
-    	while (it.hasNext()) {
-    		Map.Entry<String, PackageInfo> pair = (Map.Entry<String, PackageInfo>) it.next();
-    		PackageInfo info = pair.getValue();
-    		Map<Integer,ResultData> buildResults = info.getBuildPackageResults();
-    		if (buildResults.keySet().size() >= 2) {// need at least two builds to be able to compare
-    			Iterator resultIt = buildResults.entrySet().iterator();
-    			Map.Entry<Integer, ResultData> firstEntry = (Entry<Integer, ResultData>) resultIt.next();
-    			Map.Entry<Integer, ResultData> secondEntry = (Entry<Integer, ResultData>) resultIt.next();
-    			ResultData firstBuild = firstEntry.getValue();
-    			ResultData secondBuild = secondEntry.getValue();
-    			if (firstBuild.isPassed() != secondBuild.isPassed()) {
-    				ret += firstBuild.getName() + " changed.\n";
-    			}
+        String ret = "Last two builds: \n";
+        Map<String,PackageInfo> results = resultInfo.getPackageResults();
+        Iterator it = results.keySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, PackageInfo> pair = (Map.Entry<String, PackageInfo>) it.next();
+            PackageInfo info = pair.getValue();
+            Map<Integer,ResultData> buildResults = info.getBuildPackageResults();
+            if (buildResults.keySet().size() >= 2) {// need at least two builds to be able to compare
+                Iterator resultIt = buildResults.entrySet().iterator();
+                Map.Entry<Integer, ResultData> firstEntry = (Entry<Integer, ResultData>) resultIt.next();
+                Map.Entry<Integer, ResultData> secondEntry = (Entry<Integer, ResultData>) resultIt.next();
+                ResultData firstBuild = firstEntry.getValue();
+                ResultData secondBuild = secondEntry.getValue();
+                if (firstBuild.isPassed() != secondBuild.isPassed()) {
+                    ret += firstBuild.getName() + " changed.\n";
+                }
 
-    		}
-    	}
-    	return ret;
+            }
+        }
+        return ret;
     }
 //>>>>>>> .r18848
 }
--- src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
+++ src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
@@ -12,60 +12,60 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 public class TestResultsAnalyzerActionTest {
-	@Rule
-	public JenkinsRule j = new JenkinsRule();
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
 
-	@Test
-	public void testGetModuleList() throws Exception {
-		
-		assertTrue(true);
-	}
+    @Test
+    public void testGetModuleList() throws Exception {
+        
+        assertTrue(true);
+    }
 
-	@Test
-	public void testGetTreeResult1() throws Exception {
-		FreeStyleProject project = j.createFreeStyleProject();
-		FreeStyleBuild build = project.scheduleBuild2(0).get();
-		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
+    @Test
+    public void testGetTreeResult1() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        FreeStyleBuild build2 = project.scheduleBuild2(0).get();
 
-		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
-				project);
-		action.getJsonLoadData();
-		JSONObject jsonObj = action.getTreeResult("2", "PASSED");
-		System.out.println(jsonObj.toString());
-		assertEquals("{\"builds\":[\"2\",\"1\"],\"results\":[]}",
-				jsonObj.toString());
-	}
+        TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
+                project);
+        action.getJsonLoadData();
+        JSONObject jsonObj = action.getTreeResult("2", "PASSED");
+        System.out.println(jsonObj.toString());
+        assertEquals("{\"builds\":[\"2\",\"1\"],\"results\":[]}",
+                jsonObj.toString());
+    }
 
-	@Test
-	public void testGetTreeResult2() throws Exception {
-		FreeStyleProject project = j.createFreeStyleProject();
-		FreeStyleBuild build = project.scheduleBuild2(0).get();
-		FreeStyleBuild build2 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build3 = project.scheduleBuild2(0).get();
-		FreeStyleBuild build4 = project.scheduleBuild2(0).get();
+    @Test
+    public void testGetTreeResult2() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        FreeStyleBuild build = project.scheduleBuild2(0).get();
+        FreeStyleBuild build2 = project.scheduleBuild2(0).get();
+        FreeStyleBuild build3 = project.scheduleBuild2(0).get();
+        FreeStyleBuild build4 = project.scheduleBuild2(0).get();
 
-		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
-				project);
-		action.getJsonLoadData();
+        TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
+                project);
+        action.getJsonLoadData();
 
-		JSONObject jsonObj = action.getTreeResult("2", "all");
-		assertEquals("{\"builds\":[\"4\",\"3\"],\"results\":[]}",
-				jsonObj.toString());
-		jsonObj = action.getTreeResult("5", "all");
-		assertEquals("{\"builds\":[\"4\",\"3\",\"2\",\"1\"],\"results\":[]}",
-				jsonObj.toString());
-	}
+        JSONObject jsonObj = action.getTreeResult("2", "all");
+        assertEquals("{\"builds\":[\"4\",\"3\"],\"results\":[]}",
+                jsonObj.toString());
+        jsonObj = action.getTreeResult("5", "all");
+        assertEquals("{\"builds\":[\"4\",\"3\",\"2\",\"1\"],\"results\":[]}",
+                jsonObj.toString());
+    }
 
-	@Test
-	public void testGetLastTwoBuilds() throws Exception {
-		FreeStyleProject project = j.createFreeStyleProject();
+    @Test
+    public void testGetLastTwoBuilds() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
 
-		TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
-				project);
-		action.resultInfo = new ResultInfo();
+        TestResultsAnalyzerAction action = new TestResultsAnalyzerAction(
+                project);
+        action.resultInfo = new ResultInfo();
 
-		System.out.println(action.getLastTwoBuilds());
-	}
-	
+        System.out.println(action.getLastTwoBuilds());
+    }
+    
 
 }
