734b5123148a5de82f9c12181906258f0da2c8b2
goverdh2 <goverdh2@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-07 19:59:33 +0000

    source code button goverdh2

--- src/main/webapp/js/test-result-analyzer-template.js
+++ src/main/webapp/js/test-result-analyzer-template.js
@@ -13,7 +13,7 @@ var tableContent = '<div class="table-row {{parentclass}}-{{addName text}}" pare
         '>&nbsp;{{text}}</div>' +
     '' +
     '{{#each this.buildResults}}' +
-    '\n' + '         <div class="table-cell build-result {{applystatus status}}" data-result=\'{{JSON2string this}}\'><a href="{{createURL buildNumber ..}}">{{applyvalue status totalTimeTaken}}</a></div>' +
+    '\n' + '         <div class="table-cell build-result {{applystatus status}}" data-result=\'{{JSON2string this}}\'><button type="button" class="btn btn-primary btn-xs">Source Code</button><a href="{{createURL buildNumber ..}}">{{applyvalue status totalTimeTaken}}</a></div>' +
     '{{/each}}' +
     '\n' + '</div>' +
     '{{#each children}}\n' +
