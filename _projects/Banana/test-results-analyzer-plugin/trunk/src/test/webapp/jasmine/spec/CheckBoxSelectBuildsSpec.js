abe51ba7d98edfa07ccc05662674ff4cfad9feb1
czhang49 <czhang49@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-07 01:10:50 +0000

    making some modification on test DOM elements

--- _projects/Banana/test-results-analyzer-plugin/trunk/src/test/webapp/jasmine/spec/CheckBoxSelectBuildsSpec.js
+++ _projects/Banana/test-results-analyzer-plugin/trunk/src/test/webapp/jasmine/spec/CheckBoxSelectBuildsSpec.js
@@ -2,69 +2,10 @@ describe("CheckBox Event Tests", function() {
   // This fullDepthJSON is borrowed from elsewhere
   var testCaseName = 'CheckBoxEventTests';
 
-  var fullDepthJSON = {
-      builds: ["1", "0"],
-      results: [ {
-          //buildResults: Array[29]
-          buildStatuses: ["PASSED"],
-          children: [ {
-              //buildResults: Array[29]
-              buildStatuses: ["PASSED"],
-              children: [ {
-                  buildResults: [
-                      {
-                          buildNumber: "1",
-                          children: [],
-                          isPassed: true,
-                          isSkipped: false,
-                          name: testCaseName,
-                          status: "PASSED",
-                          totalFailed: 0,
-                          totalPassed: 1,
-                          totalSkipped: 0,
-                          totalTests: 1,
-                          totalTimeTaken: 0
-                      },
-                      {
-                          buildNumber: "0",
-                          children: [],
-                          isPassed: false,
-                          isSkipped: false,
-                          name: testCaseName,
-                          status: "FAILED",
-                          totalFailed: 1,
-                          totalPassed: 0,
-                          totalSkipped: 0,
-                          totalTests: 1,
-                          totalTimeTaken: 0
-                      }
-                  ],
-                  buildStatuses: ["PASSED"],
-                  children: [],
-                  hierarchyLevel: 2,
-                  isChild: true,
-                  parentclass: "base-Test_Package-TestClass",
-                  parentname: "TestClass",
-                  text: testCaseName,
-                  type: "package"
-              } ],
-              hierarchyLevel: 1,
-              isChild: true,
-              parentclass: "base-Test_Package",
-              parentname: "Test.Package",
-              text: "TestClass",
-              type: "package"
-          } ],
-          parentclass: "base",
-          parentname: "base",
-          text: "Test.Package",
-          type: "package"
-      } ]
-  };
 
 //  var spyEvent;
   // This checks if the selected boxes are selected
- it ("should check the selected boxes", function() {
+/* it ("should check the selected boxes", function() {
     // This following two methods are basically doing the same thing
     //$('#1').trigger( "click" );
     //spyEvent = spyOnEvent('#1', 'click');
@@ -72,10 +13,10 @@ describe("CheckBox Event Tests", function() {
     //expect('click').toHaveBeenTriggeredOn($("#1"));
     //expect(spyEvent).toHaveBeenTriggered();
     expect(1).toEqual(1);
-  });
+  }); */
 
   // This checks if click on compare results, the needed builds are shown.
-  describe( "Check Compare results event tests", function () {
+/*  describe( "Check Compare results event tests", function () {
 
     //showCheckedBuilds();
     it ("should show only the selected boxes", function(){
@@ -85,6 +26,6 @@ describe("CheckBox Event Tests", function() {
       expect(1).toEqual(1);
     });
 
-  });
+  });*/
 
 });
