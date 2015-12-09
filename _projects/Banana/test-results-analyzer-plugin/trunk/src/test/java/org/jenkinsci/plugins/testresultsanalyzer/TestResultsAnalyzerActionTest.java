c23d0ff848167f0ce47f88c5b38f16af0528e65a
yyue7 <yyue7@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-09 02:55:43 +0000

    Refactor: removed comments

--- _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
+++ _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
@@ -24,17 +24,8 @@ public class TestResultsAnalyzerActionTest{
 
     @Test
     public void testGetModuleList() throws Exception {
-//        j.configureMaven3();
-        //URL classURL = (URL) (getClass().getProtectionDomain().getCodeSource().getLocation()+"../../../src/test/sample-project.zip");
-        //System.out.println("Class URL: " + classURL);
-        //assertEquals("", classURL);jobs/banana/workspace/trunk/src/test/sample-project.zip 
-        //expected:<> but was:<file:/home/sapin2/.jenkins/jobs/banana/workspace/trunk/target/test-classes/>
-        //MavenModuleSet project = j.createMavenProject("project1");
         FreeStyleProject project = j.createFreeStyleProject("project1");
         project.setScm(new ExtractResourceSCM(getClass().getResource("/sample-project.zip")));
-
-//        project.setScm(new SingleFileSCM ("build.xml", getClass().getResource("build.xml")));
-//        assertBuildStatusSuccess(project.scheduleBuild2(0));
         assertTrue(true);
     }
 
--- _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/moduleTest.java
+++ _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/moduleTest.java
@@ -14,7 +14,6 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.ExtractResourceSCM;
 import org.jvnet.hudson.test.SingleFileSCM;
-//import org.jvnet.hudson.test.HudsonTestCase; 
 
 import java.net.URL;
 
@@ -23,17 +22,8 @@ public class moduleTest {
     public JenkinsRule j = new JenkinsRule();
 
     public void testGetModuleList() throws Exception {
-//        j.configureMaven3();
-        //URL classURL = (URL) (getClass().getProtectionDomain().getCodeSource().getLocation()+"../../../src/test/sample-project.zip");
-        //System.out.println("Class URL: " + classURL);
-        //assertEquals("", classURL);jobs/banana/workspace/trunk/src/test/sample-project.zip 
-        //expected:<> but was:<file:/home/sapin2/.jenkins/jobs/banana/workspace/trunk/target/test-classes/>
-        //MavenModuleSet project = j.createMavenProject("project1");
         FreeStyleProject project = j.createFreeStyleProject("project1");
         project.setScm(new ExtractResourceSCM(getClass().getResource("/sample-project.zip")));
-
-//        project.setScm(new SingleFileSCM ("build.xml", getClass().getResource("build.xml")));
-//        assertBuildStatusSuccess(project.scheduleBuild2(0));
         assertTrue(true);
     }
 }
