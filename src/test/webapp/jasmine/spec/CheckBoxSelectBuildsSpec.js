a351ae79924893e1e29b6291cf7330ecb949d2f5
czhang49 <czhang49@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-09 02:53:28 +0000

    final change

--- src/test/webapp/jasmine/spec/CheckBoxSelectBuildsSpec.js
+++ src/test/webapp/jasmine/spec/CheckBoxSelectBuildsSpec.js
@@ -22,17 +22,6 @@ describe("CheckBox Event Tests", function() {
   var testchecked2=[2,4,5];
   var testresults=["result1","result2","result3","result4","result5"];
 
-  // This checks if the array is removed correctly
-  /*it ("should return the correct removed array", function() {
-    var returnedarray=removeFromArray(testarray,testchecked1);
-    console.log(returnedarray);
-    expect(returnedarray).toEqual(["8"]);
-  });
-
-  it ("should show the compare builds box is activated", function() {
-    var returnedarray2=removeFromArray(testarray,testchecked2);
-    expect(returnedarray2).toEqual([ ]);
-  });*/
 
 });
 
