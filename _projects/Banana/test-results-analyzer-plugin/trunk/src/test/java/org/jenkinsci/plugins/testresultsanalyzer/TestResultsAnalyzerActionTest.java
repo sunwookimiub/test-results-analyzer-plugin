eccb9d5b90dfbe2d722fa3dd472e27dd43cd7dd1
kim392 <kim392@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-09 02:04:19 +0000

    Imported import org.jvnet.hudson.test.HudsonTestCase kim392

--- _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
+++ _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.ExtractResourceSCM;
 import org.jvnet.hudson.test.SingleFileSCM;
+import org.jvnet.hudson.test.HudsonTestCase; 
 
 import java.net.URL;
 
