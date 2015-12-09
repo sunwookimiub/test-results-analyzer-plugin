1c319714398392ca52a2f4b8262c3828f496c78f
kim392 <kim392@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-09 02:32:50 +0000

    Wrote extends HudsonTestCase kim392

--- _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/moduleTest.java
+++ _projects/Banana/test-results-analyzer-plugin/trunk/src/test/java/org/jenkinsci/plugins/testresultsanalyzer/moduleTest.java
@@ -14,26 +14,26 @@ import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.ExtractResourceSCM;
 import org.jvnet.hudson.test.SingleFileSCM;
-//import org.jvnet.hudson.test.HudsonTestCase; 
+import org.jvnet.hudson.test.HudsonTestCase; 
 
 import java.net.URL;
 
-public class moduleTest {
+public class moduleTest extends HudsonTestCase{
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
     public void testGetModuleList() throws Exception {
-//        j.configureMaven3();
+        j.configureMaven3();
         //URL classURL = (URL) (getClass().getProtectionDomain().getCodeSource().getLocation()+"../../../src/test/sample-project.zip");
         //System.out.println("Class URL: " + classURL);
         //assertEquals("", classURL);jobs/banana/workspace/trunk/src/test/sample-project.zip 
         //expected:<> but was:<file:/home/sapin2/.jenkins/jobs/banana/workspace/trunk/target/test-classes/>
-        //MavenModuleSet project = j.createMavenProject("project1");
-        FreeStyleProject project = j.createFreeStyleProject("project1");
+        MavenModuleSet project = j.createMavenProject("project1");
+//        FreeStyleProject project = j.createFreeStyleProject("project1");
         project.setScm(new ExtractResourceSCM(getClass().getResource("/sample-project.zip")));
 
 //        project.setScm(new SingleFileSCM ("build.xml", getClass().getResource("build.xml")));
-//        assertBuildStatusSuccess(project.scheduleBuild2(0));
+        assertBuildStatusSuccess(project.scheduleBuild2(0));
         assertTrue(true);
     }
 }
