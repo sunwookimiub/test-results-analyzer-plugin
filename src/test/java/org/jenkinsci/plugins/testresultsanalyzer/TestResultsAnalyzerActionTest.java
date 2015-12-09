26edbfbce8b926ace370a16a093da01cb0741947
kim392 <kim392@34e3e05a-4eee-4517-9d54-15d958f040ce>
2015-12-09 02:17:51 +0000

    Fixed syntax error kim392

--- src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
+++ src/test/java/org/jenkinsci/plugins/testresultsanalyzer/TestResultsAnalyzerActionTest.java
@@ -30,8 +30,9 @@ public class TestResultsAnalyzerActionTest{
         //assertEquals("", classURL);jobs/banana/workspace/trunk/src/test/sample-project.zip 
         //expected:<> but was:<file:/home/sapin2/.jenkins/jobs/banana/workspace/trunk/target/test-classes/>
         //MavenModuleSet project = j.createMavenProject("project1");
-        FreeStyleProject project = j.createMavenProject("project1");
-        project.setScm(new ExtractResourceSCM(getClass().getResource("/sample-project.zip"))); 
+        FreeStyleProject project = j.createFreeStyleProject("project1");
+        project.setScm(new ExtractResourceSCM(getClass().getResource("/sample-project.zip")));
+
 //        project.setScm(new SingleFileSCM ("build.xml", getClass().getResource("build.xml")));
 //        assertBuildStatusSuccess(project.scheduleBuild2(0));
         assertTrue(true);
